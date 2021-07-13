52d7cd0d70bf19589bd9ec422efccb960613659b
abhi-moglix <abhishek.kumar11@moglix.com>
2021-07-13 20:52:09 +0530

    stock updates

---
+++ assets/js/courses/algo/react and redux/5. Stateless Functional Components/index.js
@@ -0,0 +1,15 @@
+/**
+
+1.Section Into :-
+    - Functional Components
+    - Class based Components
+
+2.Stateless Functional Component :-
+    - It is statess matlb ye class component ki tarah state manage nahi krta hai
+    - also called dumb conponent kyoki isko bas data do aur ye render ho jata hai
+    - also called presentational component
+    - Functional based components are faster than class based components.
+
+3.
+
+*/
